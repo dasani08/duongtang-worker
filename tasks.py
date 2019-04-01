da3b869a6680f18a63d78c0d24a1dc36ec8f5c24
Dang Cong Thanh <ThanhDC5@fsoft.com.vn>
2019-04-01 13:29:30 +0700

    Update task list

--- tasks.py
+++ tasks.py
@@ -1,16 +1,42 @@
 import os
 import time
 from celery import Celery
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 env = os.environ
 CELERY_BROKER_URL = env.get(
     'CELERY_BROKER_URL', 'amqp://worker:duongtang2019@localhost/duongtang')
+SQLALCHEMY_DATABASE_URI = env.get(
+    'SQLALCHEMY_DATABASE_URI',
+    'mysql+pymysql://root:duongtang2019@127.0.0.1/duongtang?charset=utf8')
 
-celery = Celery('tasks',
-                broker=CELERY_BROKER_URL)
+app = Celery('tasks',
+             broker=CELERY_BROKER_URL)
 
 
-@celery.task(name='duongtang.add')
+def get_engine_session():
+    Session = sessionmaker()
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    Session.configure(bind=engine)
+    return Session()
+
+
+class DbTask(app.Task):
+    _session = None
+
+    def after_return(self, *args, **kargs):
+        if self._session is not None:
+            self._session.remove()
+
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = get_engine_session()
+        return self._session
+
+
+@app.task(base=DbTask, name='duongtang.add', bind=True)
 def add(x, y):
     # lets sleep for a while before doing the gigantic addition task!
     time.sleep(5)
